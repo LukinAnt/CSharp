75661a6dfeffa90ea425206159658d720500360b
Lukin <global-a.lukin@rambler.ru>
2024-03-17 00:01:57 +0300

    семинар1 04

---
+++ task04/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Напишите программу, которая  на вход принимает два числа и выдает, какое больше, а какое меньше
+ 
+
+*/
+
+int max = 0;
+int min = 0;
+
+Console.Write("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if(firstNumber > secondNumber)
+{
+    max = firstNumber;
+    min = secondNumber;
+}
+else
+{
+    max = secondNumber;
+    min = firstNumber;
+}
+Console.WriteLine("max = " + max + " и " + "min = " + min);
