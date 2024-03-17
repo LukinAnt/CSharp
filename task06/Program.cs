d0606b9670a7336cbbab97b6f1fde4b9d918f517
Lukin <global-a.lukin@rambler.ru>
2024-03-17 19:01:57 +0300

    семинар1 06

---
+++ task06/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Напишите программу, которая  на вход принимает число и выдает, является ли число четным(желится ли на 2 без остатка)
+ 4
+ -3
+ 7
+*/
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int remDiv = number % 2;
+
+if(remDiv == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
