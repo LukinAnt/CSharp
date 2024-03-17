ba94cb3efcce2e1049474e807d214e231f63e0de
Lukin <global-a.lukin@rambler.ru>
2024-03-17 19:15:48 +0300

    семинар1 02

--- task02/Program.cs
+++ task02/Program.cs
@@ -1,5 +1,28 @@
 ﻿/*
 Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго
-
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
 
 */
+
+int max = 0;
+int min = 0;
+
+Console.Write("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if(firstNumber > secondNumber)
+{
+    max = firstNumber;
+    min = secondNumber;
+}
+else
+{
+    max = secondNumber;
+    min = firstNumber;
+}
+Console.WriteLine("max = " + max + " и " + "min = " + min);
