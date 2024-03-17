8b33c5b2c29f7241fd99fddfd643ae7bc185dffd
Lukin <global-a.lukin@rambler.ru>
2024-03-17 19:51:08 +0300

    семинар2 09

---
+++ task09/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+9. Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+78 -> 8
+12-> 2
+85 -> 8
+
+*/
+
+
+int number = new Random().Next(10, 100);
+Console.WriteLine(number);
+
+int first = number / 10;
+int second = number % 10;
+int max = first;
+
+if(second > first)
+{
+    max = second;
+}
+Console.WriteLine(max);
+
+// 2 var
+if(first > second)
+    Console.WriteLine(first);
+else
+    Console.WriteLine(second);
